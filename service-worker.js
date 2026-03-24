4f40a79578dff92cf0e5e36958267c07737e0f2e
eduwosha-ship-it <eduwosha@gmail.com>
2026-03-24 15:37:02 +0500

    Create service-worker.js for offline functionality

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// This is the service worker for offline functionality.
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('my-cache').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/scripts.js'
+                // Add other files you want to cache for offline functionality
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
